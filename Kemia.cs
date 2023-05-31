cc52e4c165bf79a4324e9b65fe446ce1ac9f3607
ÁkosÖtvös <akika02531@gmail.com>
2023-05-31 09:32:02 +0200

    Fél ido

---
+++ Kemia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat05
+{
+    class Kemia
+    {
+        string ev;
+        string nev;
+        string vegyjel;
+        int rendszam;
+        string felfedNev;
+
+        public Kemia(string ev, string nev, string vegyjel, int rendszam, string felfedNev)
+        {
+            this.ev = ev;
+            this.nev = nev;
+            this.vegyjel = vegyjel;
+            this.rendszam = rendszam;
+            this.felfedNev = felfedNev;
+        }
+
+        public string Ev { get => ev;}
+        public string Nev { get => nev;}
+        public string Vegyjel { get => vegyjel; }
+        public int Rendszam { get => rendszam;}
+        public string FelfedNev { get => felfedNev; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,38 @@ namespace Dolgozat05
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            List<Kemia> KemiaList = new List<Kemia>();
+            var mezok = File.ReadAllLines("felfedezesek.csv").Skip(1).ToString().Split(';');
+            foreach (var item in KemiaList)
+            {
+                Kemia uj = new Kemia(mezok[0],mezok[1],mezok[2],int.Parse(mezok[3]),mezok[4]);
+                KemiaList.Add(uj);
+            }
+
+
+
+            //3.feladat
+            Console.WriteLine(KemiaList.Count());
+
+
+            //4.feladat
+            Console.WriteLine($"Felfedezések száma az ókorban:{KemiaList.Where(x => x.Ev.Contains("Ókor")).Count()}");
+
+            //5.feladat
+            Console.WriteLine("Kérek egy vegjelet: ");
+            string vegyjele=Console.ReadLine();
+            string HelyesVegyjel = "KR";
+            //6.feladat
+
+
+            if (KemiaList.Any(x => x.Vegyjel.ToLower() == HelyesVegyjel.ToLower()) == true)
+            {
+                KemiaList.Where(x => x.Vegyjel.ToLower() == HelyesVegyjel.ToLower()).Select(x=> x.Ev).ToList();
+            }
+
 
         }
     }
